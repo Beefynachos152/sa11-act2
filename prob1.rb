be61c0b73f508f9021a02235c9b31fceaaf47147
Christopher Landaverde <christopher4780@gmail.com>
2024-03-24 03:36:06 -0500

    edited to work

--- prob1.rb
+++ prob1.rb
@@ -1,6 +1,6 @@
 def extract_hashtags(text)
-    pattern = /#\\w+/
+    pattern = /#\w+/
     hashtags = text.scan(pattern)
     hashtags.flatten
-end
-p extract_hashtags("Love this #beautiful day! #sunny")
+  end
+  p extract_hashtags("Love this #beautiful day! #sunny")
--- prob4.rb
+++ prob4.rb
@@ -1,6 +1,6 @@
 def valid_date?(date)
-    pattern = /^\\d{4}-\\d{2}-\\d{2}$/
+    pattern = /\A\d{4}-\d{2}-\d{2}\z/
     date.match?(pattern)
-end
-p valid_date?("2023-03-15")
-p valid_date?("15-03-2023")
+  end
+  p valid_date?("2023-03-15")
+  p valid_date?("15-03-2023")
